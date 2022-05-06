817c06b1c056387856bf97432bbc18c9137808cc
dolanzhao <neuzqd@gmail.com>
2022-05-06 05:53:26 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+// const { default: BrandPanel }=require("./components/BrandPanel.vue")
+
+// import HelloWorld from './components/HelloWorld.vue'
+import BrandPanel from './components/BrandPanel.vue'
+</script>
+
+<template>
+  <brand-panel></brand-panel>
+  <!-- <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1> -->
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
