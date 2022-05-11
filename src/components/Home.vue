b896247bb5110f5c6722bebd50f2616e8b10cd9d
dolan <neuzqd@gmail.com>
2022-05-11 11:52:21 -0400

    fix a visible issue in mobile version

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -88,7 +88,7 @@
                 </transition>
             </Popover>
             <main>
-                <div class="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+                <div class="pt-16 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
                     <div class="mx-auto max-w-7xl lg:px-8">
                         <div class="lg:grid lg:grid-cols-2 lg:gap-8">
                             <div
